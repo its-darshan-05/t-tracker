716da492aa9451e57559bac3ab5354287a2d47cf
its-darshan-05 <drshnbharathi@gmail.com>
2026-02-13 05:07:43 +0530

    Fixed render port issue 2

--- server/index.js
+++ server/index.js
@@ -16,4 +16,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
 
