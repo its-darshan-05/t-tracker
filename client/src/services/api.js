ef6e3a0c19e44a71a506a2039c80d548f08ba3c6
its-darshan-05 <drshnbharathi@gmail.com>
2026-02-13 08:54:48 +0530

    Fixed render port issue 3

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://t-tracker-backend.onrender.com"
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
